e66a90c438c9d1ecdc576e1a96c3b1ac6a009e9d
Hossain <dhossainali@gmail.com>
2026-04-26 21:29:51 +0200

    asyncronized function was used to fetch the data

--- src/GithubUser.jsx
+++ src/GithubUser.jsx
@@ -2,23 +2,37 @@ import { useEffect, useState } from "react"
 
 export default function GithubUser({ username }) {
     const [data, setData] = useState(null)
-    useEffect(() => {
-        if (!username)return;
-        fetch(`https://api.github.com/users/${username}`).then(response => {
-            return response.json()
-        }).then(json => {
+    const [error, setError] = useState(null)
 
+    async function fetchAPI(username) {
+        try {
+            if (!username)return;
+            const response = await fetch(`https://api.github.com/users/${username}`)
+            if(!response.ok) {
+                throw new Error('User not found')
+            }
+            const json = await response.json()
             setData(json)
-        }).catch(error => console.error(error))
-    }, [username])
+        }
+        catch (error) {
+            setData(error);
+            console.error(error)
+        }}
 
-    if (!data) {
-        return <p>Loading...</p>
+        useEffect(() => {
+            setData(null)
+            setError(null)
+            fetchAPI(username)
+        }, [username])
+        if (error) {
+            return <p>Error: {error.message}</p>}
+        if (!data) {
+            return <p>Loading...</p>
+        }
+        return (
+            <div>
+                <img src={data.avatar_url} width={100} alt="avatar" />
+                <p>Name: {data.name}</p>
+                <p>Login name: {data.login}</p>
+            </div>)
     }
-    return (
-        <div>
-            <img src={data.avatar_url} width={100} alt="avatar"/>
-            <p>Name: {data.name}</p>
-            <p>Login name: {data.login}</p>
-        </div>)
-}
